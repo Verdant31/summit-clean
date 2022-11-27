0409f256c33ec2373929d425551e1789ab82113a
Verdant31 <joao.pedro3110@hotmail.com>
2022-11-27 13:46:06 -0300

    wip

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-render(<h1>Hello world!</h1>, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<h1>Hello world!</h1>);
 
 reportWebVitals();
